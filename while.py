a80659e6670ab48c83f0d0403e71ae88224e66b4
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2023-12-24 15:19:03 +0530

    first commit

---
+++ while.py
@@ -0,0 +1,19 @@
+'''i=0
+while(i<3):
+    print(i)
+    i=i+3
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print('i am inside else')'''
+
+
+i=int(input('enter the number'))   ## taking input from user 
+while(i<=39):                      # checking for condition
+    i=int(input('enter the number'))
+    print(i)
+print('done with loop')    
+    
