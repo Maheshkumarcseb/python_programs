be02cd00457dc69913af8fe3df64ba18a2dc7f4e
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2023-12-24 15:52:13 +0530

    first commit

---
+++ gMean.py
@@ -0,0 +1,17 @@
+def calculateGmean(a, b):
+    mean=(a*b)/(a+b)
+    print(mean)
+def isGreater(a, b):
+    if(a>b):
+        print('first no. is greater')
+    else:
+        print('seconf no. is equal to or greater')
+#def isLesser(a, b)
+a=9
+b=8
+isGreater(a, b)
+calculateGmean(a, b)
+c=5
+d=6
+isGreater(c, d)
+calculateGmean(c, d)
