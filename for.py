6e91fdca0dfa4befc48f541346878eb2198f76cb
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2023-12-24 15:21:51 +0530

    first commit

---
+++ for.py
@@ -0,0 +1,30 @@
+'''''name='kumar'
+for i in name:
+   print(i)
+
+name='kumar'
+for i in name:
+    print(name)
+
+colors=['red','green','blue','yellow']
+for color in colors:
+    print(color)
+
+colors=['red','green','blue','yellow']
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for k in range(1,9):
+    #print(k)
+    #print(k+1)
+    print(k+2)
+
+for k in range(5):
+    #print(k)
+    print(k+1) '''
+
+for k in range(1, 12, 2):
+    print(k)
+    
